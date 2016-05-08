ab4136ad2f2a71bfcc77ec8707dd0178b4d5d351
Coder-Li <858582381@qq.com>
2016-05-08 22:15:39 +0800

    checkUrl
    完成url的认证工作，并且把这个方法独立出来

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var util = require('util');
-
-var jsSHA = require('jssha');
-
-
-var token = 'test';
+var UtilCheck = require('../util/checkUrl');
+// var util = require('util');
 
 /* GET home page. */
 // /?signature=c9fab82fe4f95585d46657934a470bd37160d196&echostr=2439227987601620991&timestamp=1462713885&nonce=1620159903
-router.get('/', function(req, res, next) {
-  // console.log('the req is:\n' + util.inspect(req));
-  var signature = req.query.signature;
-  var timestamp = req.query.timestamp;
-  var nonce = req.query.nonce;
+router.get('/', function (req, res, next) {
   var echostr = req.query.echostr;
-  
-  var oriArray = new Array();
-  oriArray[0] = nonce;
-  oriArray[1] = timestamp;
-  oriArray[2] = token;
-  
-  oriArray.sort();
-  console.log(oriArray)
-  
-  var original = oriArray.join('');
-  console.log('original:');
-  console.log(original);
-  var shaObj = new jsSHA('SHA-1', 'TEXT');
-  shaObj.update(original);
-  console.log('shaObj:')
-  console.log(shaObj)
-  var scyptoString=shaObj.getHash('HEX'); 
-  
-  console.log('signature:');
-  console.log(signature);
-  console.log('scyptoString:');
-  console.log(scyptoString);
-  
-  if(signature === scyptoString){
-    //chenggong
-  res.end(echostr);
+  //如果认证成功就返回 echostr
+  if (UtilCheck.checkUrl(req, res, next)) {
+    res.end(echostr);
   }
-  // 以上是判断url
 });
-
 module.exports = router;
